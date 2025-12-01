f77d7db0a96ea3ef21161f55bc52224a7d699472
goCAD-GmbH <yannik@gocad.de>
2025-12-01 13:57:07 +0100

    default visualization code

---
+++ code/read_data.py
@@ -0,0 +1,14 @@
+import matplotlib
+matplotlib.use("QtAgg")  # ensures external interactive window
+
+import warnings
+warnings.filterwarnings("ignore", message="QObject::connect")  # optional
+
+import mne
+mne.set_config('MNE_BROWSER_THEME', 'light', set_env=True)
+raw = mne.io.read_raw_bdf(
+    # "D:\EEG\data\sub-31\eeg\sub-31_task-RPS_eeg.bdf",
+    "/Users/yannikfruehwirth/Desktop/academia/semester4/EEG/EEG/data/sub-31/eeg/sub-31_task-RPS_eeg.bdf",
+    preload=False
+)
+raw.plot(block=True)  # will use the Matplotlib-based viewer
